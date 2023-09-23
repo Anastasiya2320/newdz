479bdceed35a0915af82fecf1af3d60aedd34979
Your Name <bes6chastik@yandex.ru>
2023-09-23 10:45:48 +0300

    newdz

---
+++ new dz/задание 2 дз 1.py
@@ -0,0 +1,10 @@
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+f = float(input())
+if f - d != 0:
+    vir = (a * b - c) / (f - d)
+    print(vir)
+else:
+    print('Делить на 0 нельзя!')    
