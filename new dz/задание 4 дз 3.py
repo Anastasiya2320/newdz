43c0853618a07d36a79778d94a4abbb1a486c463
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-14 14:48:07 +0300

    Update задание 4 дз 3.py

--- new dz/задание 4 дз 3.py
+++ new dz/задание 4 дз 3.py
@@ -1,19 +1,11 @@
 from collections import Counter
-from задание 1 дз 3 import f
-def f(x):
-    while x:
-        list.append(x)
-        x = input("Ввод: ")
-    lst = []
+from задание 1 дз 3 import fy
+
+
+def ff(x):
     print("Элемент | Частота")
-    c = Counter(a)
-    return c
-    #for t in list:
-        #if t not in lst:
-        #    print(t, "|", list.count(t))
-        #   lst.append(t)
-
-
-x = input("Ввод: ")
-v = f(x)
+    return Counter(x)
+     
+x = fy(n)
+v = ff(x)
 print(*[f'{i} | {v[i]}' for i in v] , sep = '\n')
