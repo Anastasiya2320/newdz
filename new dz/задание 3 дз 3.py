b29bbf0fc1e57a55a0a975b0bec5f6196c715bc9
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-14 14:11:41 +0300

    Update задание 3 дз 3.py

--- new dz/задание 3 дз 3.py
+++ new dz/задание 3 дз 3.py
@@ -1,7 +1,7 @@
 def f(n):
     fib = [0, 1]
     for i in range(2, n):
-        fib.append(fib[i-1] + fib[i-2])
+        fib.append(fib[-1] + fib[-2])
     print(' '.join(str(x) for x in fib[:n]))
 
 n = int(input("Введите количество чисел Фибоначчи: "))
