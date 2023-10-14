f3834d4f18da3028938c15bd1a8b2ceb71dc4711
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-14 14:52:16 +0300

    Update задание 2 дз 3.py

--- new dz/задание 2 дз 3.py
+++ new dz/задание 2 дз 3.py
@@ -1,13 +1,12 @@
-def Season(n):
+def season(n):
     if n in [1, 2, 12]:
-        return ("Зима")
-    elif n in [3, 4, 5]:
-        return ("Весна")
-    elif n in [6, 7, 8]:
-        return ("Лето")
-    else:
-        return ("Осень")
+        return "Зима"
+    if n in [3, 4, 5]:
+        return "Весна"
+    if n in [6, 7, 8]:
+        return "Лето"
+    return "Осень"
 
 
 n = int(input("Месяц: "))
-Season(n)
+season(n)
