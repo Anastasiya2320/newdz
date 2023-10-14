4c7aa578d164ccd365fa930b196637e0a740771a
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-14 14:55:59 +0300

    Update задание 6 дз 3.py

--- new dz/задание 6 дз 3.py
+++ new dz/задание 6 дз 3.py
@@ -1,7 +1,7 @@
-def f (n): 
+def f(n): 
     if n < 2:
         return False
-    for i in range(2, int(n ** 0.5)+1):
+    for i in range(2, int(n ** 0.5) + 1):
         if not n % i:
             return False
     return True
