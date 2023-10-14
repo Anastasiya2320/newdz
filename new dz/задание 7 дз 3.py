5c4893f0792e61253c8993b535e226c9c7c5930f
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-14 14:00:34 +0300

    Update задание 7 дз 3.py

--- new dz/задание 7 дз 3.py
+++ new dz/задание 7 дз 3.py
@@ -3,8 +3,8 @@ from collections import Counter
 text = input().split()
 
 def f(text):
-    u = max(text, key =len)
+    u = max(text, key = len)
     c = Counter(text).most_common(1)[0][0]
     a = max(c, key = lambda x: c[x])
-    print('самое длиное:' + u + "самое частое:" + a )
+    print('самое длиное:' + u + "самое частое:" + c )
 f(text)
