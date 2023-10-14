dadf3f43597ab2cc322fec11565b2a9b28d96009
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-14 12:04:39 +0300

    Add files via upload

---
+++ new dz/задание 3 дз 3.py
@@ -0,0 +1,8 @@
+def f(n):
+    fib = [0, 1]
+    for i in range(2, n):
+        fib.append(fib[i-1] + fib[i-2])
+    print(' '.join(str(x) for x in fib[:n]))
+
+n = int(input("Введите количество чисел Фибоначчи: "))
+f(n)
