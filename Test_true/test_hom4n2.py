c80d070b9aa4e1f05c1b85455b842d5ff1515b54
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2023-10-27 22:20:24 +0300

    Add files via upload

---
+++ Test_true/test_hom4n2.py
@@ -0,0 +1,13 @@
+from newdz.hom4n2 import cn
+
+
+@pytest.mark.parametrize(
+    ('n', 'end'),
+    [
+        (5, 120),
+        (4, 24),
+        (0, 1)
+    ]
+)
+def test(n, end):
+    assert cn(n) == end
---
+++ Test_true/test_hom4n3.py
@@ -0,0 +1,24 @@
+import pytest
+from newdz.hom4n3 import notsame
+
+
+@pytest.mark.parametrize(
+    ('x'),
+    [
+        [True, 1],
+        [555]
+    ]
+)
+def test(x):
+    assert notsame(x) is True
+
+
+@pytest.mark.parametrize(
+    ('x'),
+    [
+        [1, 1],
+        [1, 2, 5, 1]
+    ]
+)
+def testf(x):
+    assert notsame(x) is False
