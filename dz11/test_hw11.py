f43afae5dcd3c50f432cd49ffaa428d90e57f861
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2024-02-24 14:49:40 +0300

    Update test_hw11.py

--- dz11/test_hw11.py
+++ dz11/test_hw11.py
@@ -1,4 +1,18 @@
+import pytest
 from dz11.hw11 import Time
+
+@pytest.mark.parametrize("hours, minutes, seconds, expected_hours, expected_minutes, expected_seconds", [
+    (10, 30, 45, 10, 30, 45),  # Valid input values
+    (25, 70, -5, 0, 0, 0),      # Invalid input values
+])
+def test_input_time(hours, minutes, seconds, expected_hours, expected_minutes, expected_seconds):
+    t = Time()
+    t.h = hours
+    t.m = minutes
+    t.s = seconds
+    assert t.h == expected_hours
+    assert t.m == expected_minutes
+    assert t.s == expected_seconds
 
 def test_input_time(mocker):
     mocker.patch("builtins.input", return_value = 1)
