551d32194047e954721be37e113da4befee69097
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2024-02-24 11:27:49 +0300

    Add files via upload

---
+++ dz11/test_hw11.py
@@ -0,0 +1,40 @@
+from dz11.hw11 import Time
+
+def test_input_time(mocker):
+    mocker.patch("builtins.input", return_value = 1)
+    t = Time()
+    t.input_time()
+    assert t.h == 1
+    assert t.m == 1
+    assert t.s == 1
+
+
+def test_str_(mocker):
+    t = Time(13, 5, 34)
+    assert str(t) == 'время: 13:05:34'
+   
+   
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
