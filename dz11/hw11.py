05f19bd8298edde8211315acdca388045ee510f2
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2024-02-24 11:25:56 +0300

    Update hw11.py

--- dz11/hw11.py
+++ dz11/hw11.py
@@ -5,9 +5,14 @@ class Time:
         self.s = s
 
     def input_time(self):
-        self.h = int(input("введите часы: "))
-        self.m = int(input("введите минуты: "))
-        self.s = int(input("введите секунды: "))
+        while True:
+            self.h = int(input("введите часы: "))
+            self.m = int(input("введите минуты: "))
+            self.s = int(input("введите секунды: "))
+             if self.h > 23 or self.m > 59 or self.s > 59 or self.h < 0 or self.m < 0 or self.s < 0:
+               print('ошибка')
+            else:
+               break
 
     def __str__(self):
        return f"время: {self.h:02d}:{self.m:02d}:{self.s:02d}" 
