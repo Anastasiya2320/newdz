f5d4785f5b881ebcd94471177b745a1b3008dc16
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2024-03-02 14:07:30 +0300

    Update test_hw11.py

--- dz11/test_hw11.py
+++ dz11/test_hw11.py
@@ -1,18 +1,6 @@
 import pytest
 from dz11.hw11 import Time
 
-@pytest.mark.parametrize("hours, minutes, seconds, expected_hours, expected_minutes, expected_seconds", [
-    (10, 30, 45, 10, 30, 45),  # Valid input values
-    (25, 70, -5, 0, 0, 0),      # Invalid input values
-])
-def test_input_time(hours, minutes, seconds, expected_hours, expected_minutes, expected_seconds):
-    t = Time()
-    t.h = hours
-    t.m = minutes
-    t.s = seconds
-    assert t.h == expected_hours
-    assert t.m == expected_minutes
-    assert t.s == expected_seconds
 
 def test_input_time(mocker):
     mocker.patch("builtins.input", return_value = 1)
@@ -23,13 +11,28 @@ def test_input_time(mocker):
     assert t.s == 1
 
 
-def test_str_(mocker):
+def test_init_(mocker):
     t = Time(13, 5, 34)
-    assert str(t) == 'время: 13:05:34'
-   
-   
-      
+    assert t.h == 13
+    assert t.m == 5
+    assert t.s == 34
 
+
+@pytest.mark.parametrize("hours, minutes, seconds, expected_str",
+                         [(13, 5, 34, 'время: 13:05:34'),
+                          (8, 15, 0, 'время: 08:15:00'),
+                          (1, 1, 1, 'время: 01:01:01')
+                          (23, 59, 59, 'время: 23:59:59')])
+def test_str_(hours, minutes, seconds, expected_str):
+    t = Time(hours, minutes, seconds)
+    assert str(t) == expected_str
+
+
+def test_input_time_with_invalid_values(mocker):
+    mocker.patch("builtins.input", return_value = 24)
+    t = Time()
+    with pytest.raises(ValueError):
+        t.input_time()
 
 
 
