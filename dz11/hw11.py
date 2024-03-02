c2628628b136740e954c2d418ccc5e6a665129e0
Anastasiya2320 <143869885+Anastasiya2320@users.noreply.github.com>
2024-03-02 14:06:49 +0300

    Update hw11.py

--- dz11/hw11.py
+++ dz11/hw11.py
@@ -1,25 +1,26 @@
-import sys
 class Time:
+
     def __init__(self, h = 0, m = 0, s = 0):
         self.h = h
         self.m = m
         self.s = s
 
     def input_time(self):
-        while True:
+        try:
             self.h = int(input("введите часы: "))
             self.m = int(input("введите минуты: "))
             self.s = int(input("введите секунды: "))
-             if self.h > 23 or self.m > 59 or self.s > 59 or self.h < 0 or self.m < 0 or self.s < 0:
-               print('ошибка')
-               sys.exit()  
-            else:
-               break
+        except ValueError:
+            raise ValueError("вводи нормальные значения!!!!!") 
+
+        if self.h > 23 or self.m > 59 or self.s > 59 or self.h < 0 or self.m < 0 or self.s < 0 :
+            raise ValueError ("что то не правильно")       
 
     def __str__(self):
        return f"время: {self.h:02d}:{self.m:02d}:{self.s:02d}" 
-'''    
+'''
 t = Time()
 t.input_time()
-print(t)       
+print(t)  
 '''
+
